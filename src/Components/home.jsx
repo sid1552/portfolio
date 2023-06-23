1bebada68366af6710a6ca32d411993420e22040
Siddharth Singh <siddharthsingh1552@gmail.com>
2023-06-23 11:52:14 +0530

    created HOME page

---
+++ src/Components/home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+
+function Home() {
+  const [text] = useTypewriter({
+    words: ['am a Web-Developer', 'love software development'],
+    loop: {},
+    typeSpeed: 90,
+    deleteSpeed: 70,
+  });
+
+  return (
+    <div className="home">
+      <div>
+        <h1>Hi , my name is Siddharth</h1>
+      </div>
+      <div>
+        {' '}
+        <h1>
+          I <span style={{ color: '#40B8B6' }}>{text}</span>
+          <Cursor />
+        </h1>
+      </div>
+      <div className="bio">
+        I am Siddharth Singh, a dedicated and ambitious Computer Science
+        undergraduate. Currently pursuing my Bachelor of Technology degree, I am
+        passionate about web development
+      </div>
+    </div>
+  );
+}
+
+export default Home;
--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Components/navbar';
 import DayNight from './Components/day_night';
+import Home from './Components/home';
 
 function App() {
   return (
     <div>
       <Navbar />
       <DayNight />
+      <Home />
     </div>
   );
 }
