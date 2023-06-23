42dd04c202890754bbba41ee3f67008680c9a193
Siddharth Singh <siddharthsingh1552@gmail.com>
2023-06-23 12:11:32 +0530

    Updated Day Night Button

--- src/Components/day_night.jsx
+++ src/Components/day_night.jsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function DayNight() {
-  const [isDay, setIsDay] = useState(true);
+const DayNight = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const toggleBackground = () => {
-    setIsDay(!isDay);
+  const handleClick = () => {
+    setIsDarkMode(!isDarkMode);
+
+    // Remove background-image property and set background color
+    if (isDarkMode) {
+      document.body.style.backgroundImage = "url('./assets/light.jpg')";
+      document.body.style.backgroundColor = 'transparent';
+    } else {
+      document.body.style.backgroundImage = 'none';
+      document.body.style.backgroundColor = '#2C2F35';
+    }
   };
 
+  useEffect(() => {
+    // Set initial background image and color on component mount
+    document.body.style.backgroundImage = "url('./assets/light.jpg')";
+    document.body.style.backgroundColor = 'transparent';
+  }, []);
+
   return (
-    <div>
-      <button id="bgButton" onClick={toggleBackground}>
-        {isDay ? (
-          <i className="fas fa-sun"></i>
-        ) : (
-          <i className="fas fa-moon"></i>
-        )}
-      </button>
-      <style>{`
-        body {
-          background-image: url(${
-            isDay ? './assets/light.jpg' : './assets/dark.jpg'
-          });
-          background-repeat: no-repeat;
-          background-size: cover;
-          background-position: center;
-          background-attachment: fixed;
-        }
-      `}</style>
+    <div id="bgButton" onClick={handleClick}>
+      <i
+        className={`fa ${isDarkMode ? 'fa-moon' : 'fa-sun'}`}
+        style={{ padding: '0.5rem', fontSize: '2rem', color: '#3c8a89' }}
+      />
     </div>
   );
-}
+};
 
 export default DayNight;
